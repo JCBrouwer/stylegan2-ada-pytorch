0111897aa9edb7983011a4764986aaedf06e4ce4
Hans <hans@brouwer.work>
2021-06-08 16:59:01 +0000

    formatter

--- speedtest.py
+++ speedtest.py
@@ -19,7 +19,8 @@ import sys
 import io
 
 from generate import *
-# from training.training_loop import *
+
+from training.training_loop import *
 from training.networks import *
 from torch_utils.misc import *
 from training.legacy import *
@@ -35,7 +36,7 @@ from training.legacy import *
 #     class_idx: Optional[int],
 #     projected_w: Optional[str],
 # ):
-    
+
 # class Generator(torch.nn.Module):
 #     def __init__(
 #         self,
@@ -48,46 +49,58 @@ from training.legacy import *
 #         synthesis_kwargs={},  # Arguments for SynthesisNetwork.
 #     ):
 
-    
-def speedtest(seeds: Optional[List[int]], truncation_psi: float, noise_mode: str, outdir: str, z_dim: int, c_dim: int, w_dim: int, img_resolution: int, img_channels: int, synthesis_kwargs: dict):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print()
-    print(device, torch.cuda.is_available())
-    network_pkl = "https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl"
 
-    G = Generator(z_dim = z_dim, c_dim = c_dim, w_dim = w_dim, img_resolution = img_resolution, img_channels = img_channels, synthesis_kwargs=synthesis_kwargs)
+def speedtest(
+    seeds: Optional[List[int]],
+    truncation_psi: float,
+    noise_mode: str,
+    outdir: str,
+    z_dim: int,
+    c_dim: int,
+    w_dim: int,
+    img_resolution: int,
+    img_channels: int,
+    synthesis_kwargs: dict,
+):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    G = Generator(
+        z_dim=z_dim,
+        c_dim=c_dim,
+        w_dim=w_dim,
+        img_resolution=img_resolution,
+        img_channels=img_channels,
+        synthesis_kwargs=synthesis_kwargs,
+    )
     G = G.to(device)
-    # with dnnlib.util.open_url(network_pkl) as f:
-    #     G = load_network_pkl(f)["G_ema"].to(device)  # type: ignore
-    
-    old_stdout = sys.stdout # Memorize the default stdout stream
+
+    old_stdout = sys.stdout  # Memorize the default stdout stream
     sys.stdout = buffer = io.StringIO()
     z = torch.from_numpy(np.random.RandomState(0).randn(1, G.z_dim)).to(device)
     summary = print_module_summary(G, [z, c_dim])
     sys.stdout = old_stdout
     # sys.stdout = sys.__stdout__
-    whatWasPrinted = buffer.getvalue() 
-    
+    whatWasPrinted = buffer.getvalue()
+
     label = torch.zeros([1, G.c_dim], device=device)
-    
+
     print(G.z_dim, G.c_dim, G.w_dim, G.img_resolution, G.img_channels)
-    
+
     for seed_idx, seed in enumerate(seeds):
         print("Generating image for seed %d (%d/%d) ..." % (seed, seed_idx, len(seeds)))
         z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
         img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
         img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
         PIL.Image.fromarray(img[0].cpu().numpy(), "RGB").save(f"{outdir}/seed{seed:04d}.png")
-        
-    return whatWasPrinted
 
+    return whatWasPrinted
 
 
 if __name__ == "__main__":
     torch.cuda.empty_cache()
     seeds = [i for i in range(10)]
     trunc = 1
-    outdir = 'out'
+    outdir = "out"
     noise_mode = "const"
     z_dim = 512
     c_dim = 0
@@ -96,42 +109,51 @@ if __name__ == "__main__":
     img_channels = 3
     channel_base = 32768
     channel_max = 512
-    synthesis_kwargs = {'channel_base': channel_base, 'channel_max': channel_max}
-    
-    
-    #Put into a loop, changing some of the synthesis_kwargs per round
-    #Create variable/dict to store parameter:average time
+    synthesis_kwargs = {"channel_base": channel_base, "channel_max": channel_max}
+
+    # Put into a loop, changing some of the synthesis_kwargs per round
+    # Create variable/dict to store parameter:average time
     averagetime = {}
     fullsummary = {}
     n = 2
-    channel_base_arr = np.linspace(32768/n,32768,n,dtype=int)
-    channel_max_arr = np.linspace(512/n,512,n,dtype=int)
-    #Start loop
+    channel_base_arr = np.linspace(32768 / n, 32768, n, dtype=int)
+    channel_max_arr = np.linspace(512 / n, 512, n, dtype=int)
+    # Start loop
     for i in range(n):
-        #Change channel_base and channel_max
+        # Change channel_base and channel_max
         channel_base = channel_base_arr[i]
-        channel_max = channel_max_arr[n-1]
-        #Start timer
+        channel_max = channel_max_arr[n - 1]
+        # Start timer
         timestart = time.time()
-        summary = speedtest(seeds = seeds, truncation_psi=trunc, noise_mode=noise_mode, outdir=outdir, z_dim=z_dim, c_dim=c_dim, w_dim=w_dim, img_resolution=img_resolution, img_channels=img_channels, synthesis_kwargs=synthesis_kwargs)  # pylint: disable=no-value-for-parameter
-        #End timer
+        summary = speedtest(
+            seeds=seeds,
+            truncation_psi=trunc,
+            noise_mode=noise_mode,
+            outdir=outdir,
+            z_dim=z_dim,
+            c_dim=c_dim,
+            w_dim=w_dim,
+            img_resolution=img_resolution,
+            img_channels=img_channels,
+            synthesis_kwargs=synthesis_kwargs,
+        )  # pylint: disable=no-value-for-parameter
+        # End timer
         timeend = time.time()
         torch.cuda.empty_cache()
-        
-        #Store time and kwargs in dict
+
+        # Store time and kwargs in dict
         Parameters = int(summary[-52:-44])
         Buffers = int(summary[-40:-33])
-        timeavg = (timeend-timestart)/len(seeds)
+        timeavg = (timeend - timestart) / len(seeds)
         averagetime[str(i)] = (Parameters, timeavg)
         fullsummary[str(i)] = summary
-        #End loop
+        # End loop
     torch.cuda.empty_cache()
-    #Print dict
+    # Print dict
 
-    print('test')
+    print("test")
     # print(fullsummary)
     print(averagetime)
     # Parameters = int(summary[-52:-44])
     # Buffers = int(summary[-40:-33])
-    
-    
+
